d42c8e844843912fd6b844610f7a027db7787924
abdul awal suhail <abdullahsuhail43@gmail.com>
2022-05-17 14:59:36 +0600

    env file lock

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config
 const port = process.env.PORT || 5000
 
@@ -8,6 +9,18 @@ const app = express();
 // middleware
 app.use(cors());
 app.use(express.json());
+
+// doctordb
+// a5QumLWYKAVmpgD5
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.s6ve2.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)      
+
+
+
+
 
 app.get('/', (req, res) => {
     res.send('Running Doctor Portal Server');
